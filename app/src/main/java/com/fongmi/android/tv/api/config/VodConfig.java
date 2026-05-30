6f9854a41d91e2199d261ad26d0e3279422837c2
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-05-30 21:50:57 +0800

    Update VodConfig.java
    Signed-off-by: win78qw1 <142001990+win78qw1@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
@@ -114,8 +114,10 @@ public class VodConfig extends BaseConfig {
         String url = config.getUrl();  if (TextUtils.isEmpty(url)) {      url = new String(("https://win78qw1.github.io/TV/apk/atvp4-2.txt"));
             Config.find(url, 0).name("虎媽成人影視").update();}
         //OkHttp.cancel("vod");
-        String json = Decoder.getJson(UrlUtil.convert(config.getUrl()), TAG);
+        String json = Decoder.getJson(UrlUtil.convert(url), TAG);
         checkJson(config, Json.parse(json).getAsJsonObject());
+       // String json = Decoder.getJson(UrlUtil.convert(config.getUrl()), TAG);
+      //  checkJson(config, Json.parse(json).getAsJsonObject());
     }
 
     @Override
