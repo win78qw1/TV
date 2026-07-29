13d9e23397f6e72d7692b673f2d3093b4c735b38
win78qw1 <142001990+win78qw1@users.noreply.github.com>
2026-07-29 09:54:01 +0800

    Update script.js
    Signed-off-by: win78qw1 <142001990+win78qw1@users.noreply.github.com>

--- apk/script.js
+++ apk/script.js
@@ -717,7 +717,10 @@ color:${c};
 ytpSetI.innerHTML+=`<br><b style='font-size:18px' >YOUTUBE SD Settings</b>
 <span style="font-size:10px">v${YTProVer}</span>
 <br><br>
+<div data-action="follow" style="min-height:35px;height:auto;width:95%;margin:auto;background:#ee2a7b44;border-radius:30px;margin-bottom:15px;border:1px solid #ee2a7b;display:flex;padding:5px;gap:8px;">
 
+
+<div style="display:flex;flex-direction:column;align-items:flex-start;height:100%;width:auto;flex-shrink:0;font-size:14px;background:re;padding:0;"><b>Please follow Habitius on Instagram</b>For daily habit,lifestyle and health tips </div>
 
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${isD ? "#ccc" : "#444"}" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
